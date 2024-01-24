82006ac38e8b31c64e8b6ea197539a1f8a088922
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2024-01-24 09:57:09 +0530

    created structure for 2 features
    An home page lining both the features

--- app.py
+++ app.py
@@ -6,9 +6,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/feature1')
+def feature1():
+    return render_template('feature1.html')
+
+@app.route('/feature2')
+def feature2():
     return render_template('feature2.html')
 
 
+@app.route('/process', methods=['POST'])
+def process():
+    area_region = request.form['area_region']
+    weather_data=weather.get_weather_data(area_region)
+    estimated_electricity=weather.print_weather_info(weather_data)
+    elec=str(estimated_electricity) + " kWh"
+    return render_template('feature1.html', weather_data=elec)
 
 if __name__ == '__main__':
     app.run(debug=True)
